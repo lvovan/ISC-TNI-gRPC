b304797595baa391776d9f1daa74df69928856d2
Ubuntu <lvovan@lvovan-linux-vm.2rz3ocgmsioexbsdhvkkfazcke.parx.internal.cloudapp.net>
2021-09-20 08:43:25 +0000

    Ajout de step3-fin

---
+++ step3-fin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace Calc
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            var x = int.Parse(args[0]);
+            var y = int.Parse(args[1]);
+
+            // Création du client (nous devons accepter )
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:50051");
+            var client =  new Calc.CalcClient(channel);
+
+            // Appels au serveur
+            var replyAdd = client.Add(new ArithmeticRequest { A = x, B = y });
+            var replyMultiply = client.Multiply(new ArithmeticRequest { A = x, B = y });
+            
+            // Affichage des résultats
+            Console.WriteLine($"Client .NET pour Calc");
+            Console.WriteLine($"{x} + {y} = {replyAdd.Result}");
+            Console.WriteLine($"{x} * {y} = {replyMultiply.Result}");
+        }
+    }
+}
